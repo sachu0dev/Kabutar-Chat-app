2b086b018a1c6de9d0755cfc555dad02d07a1064
sachu0dev <sushil.dev.in@gmail.com>
2024-09-17 12:47:23 +0530

    git push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
       .get(`${server}/user/me`, { withCredentials: true })
       .then((res) => dispatch(userExists(res.data.user)))
       .catch(() => dispatch(userNotExist()));
-  }, [dispatch]);
+  }, [dispatch, user]);
 
   return loader ? (
     <LayoutLoader />
